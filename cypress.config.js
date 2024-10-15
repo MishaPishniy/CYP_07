d3c70c5c1b921576fdc16f7d5b4ab16473463284
Mychailo Pyshnyi <mishap635@gmail.com>
2024-10-15 09:14:20 +0300

    new

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    chromeWebSecurity: false, // Отключить функции безопасности Chrome
+    experimentalSessionAndOrigin: true, // Включить поддержку сессий и кросс-доменных запросов
+    defaultCommandTimeout: 10000, // Время ожидания команд
+    viewportWidth: 1280,
+    viewportHeight: 720, 
+  },
+});
