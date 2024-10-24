f5f6a7b43b51681e30e5a5d9b611c2c1760db07c
MishaPishniy <mishap635@gmail.com>
2024-10-24 17:56:03 +0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,13 +2,14 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://www.saucedemo.com/",
-    env: {
-      BASE_URL: "https://www.saucedemo.com/"
-    },
     chromeWebSecurity: false,
     setupNodeEvents(on, config) {
-      // Add your Node event listeners here if needed
+      // Используем значение переменной окружения из командной строки, если оно задано
+      config.env.BASE_URL = process.env.BASE_URL || config.env.BASE_URL || "https://www.saucedemo.com/";
+      return config;
     },
+    env: {
+      BASE_URL: "https://www.saucedemo.com/"
+    }
   },
 });
